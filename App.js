5aaa67514ffc6f3f0ba2f5286b6607faf8b40e53
Federico <fjaralampidis@gmail.com>
2021-05-05 18:49:30 -0300

    The game is working properly but it needs styles to make it look nicer as well as a function to end the game

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () =>{
     
-    const cardArray = [ // theese are my cards
+    const cardArray = [ // theese are my cards, there are two of each because there must be 2 in order to match them
         {
             name: 'Azazel',
             img: 'Images/Azazel.jpg'
@@ -51,17 +51,56 @@ document.addEventListener('DOMContentLoaded', () =>{
         },
     ]
 
-    const grid = document.querySelector('.grid');
+    cardArray.sort(() => 0.5 - Math.random());
+
+    const grid = document.querySelector('.grid'); // querySelector selects an specific section from html file and converts it to an object
+    const resultDisplay = document.querySelector('#result');
+    var cardsChosen = [];
+    var cardsChosenId = [];
+    var cardsWon = [];
 
     function createBoard(){
         for(let i = 0; i < cardArray.length; i++){
             var card = document.createElement('img');
             card.setAttribute('src', 'Images/Dado.png');
             card.setAttribute('data-id', i);
-            // card.addEventListener('click', flipCard);
+            card.setAttribute("class", "images");
+            card.addEventListener('click', flipCard);
             grid.appendChild(card);
         }
     }
 
-    
+    function checkForMatch(){
+        var cards = document.querySelectorAll('img');
+        const optionOneId = cardsChosenId[0];
+        const optionTwoId = cardsChosenId[1];
+        if(cardsChosen[0] === cardsChosen[1]){
+            alert('yep');
+            
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'Images/Momia.jpg');
+            cards[optionTwoId].setAttribute('src', 'Images/Momia.jpg');
+            alert('Nope');
+        }
+        cardsChosen = [];
+        cardsChosenId = [];
+        resultDisplay.textContent = cardsWon.length;
+        if (cardsWon.length === cardsArray.length/2) {
+            resultDisplay.textContent = "Encontraste todas Wachinn !!";
+        }
+    }
+
+    function flipCard(){
+        var cardId = this.getAttribute('data-id'); //porque usa la expresion this
+        cardsChosen.push(cardArray[cardId].name);
+        cardsChosenId.push(cardId);
+        this.setAttribute('src', cardArray[cardId].img);
+        if (cardsChosen.length == 2){
+            setTimeout(checkForMatch, 500);
+
+        }
+    }
+
+    createBoard();
 })
